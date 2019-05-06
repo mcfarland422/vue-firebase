d502f52ab8e3aa55a0a7e3eefd51da88c0161722
Michael McFarland <michael.mcfarland@luckie.com>
2019-05-06 09:23:45 -0400

    first

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="app">
+    <h1>{{ title }}</h1>
+    <Navbar />
+  </div>
+</template>
+
+<script>
+import Navbar from './Navbar'
+
+export default {
+  name: 'app',
+  components: {
+    Navbar: Navbar
+  },
+  data () {
+    return {
+      title: 'Vue.js App'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+  color: #444;
+  text-align: center;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/assets/AllFriends.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="all-friends">
+        <h2>All Friends</h2>
+        <div v-for="(friend, index) in friends" :key="indes">
+            <span @click="unfriend(friend.name)">{{ friend.name }}</span>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'AllFriends',
+    props: ['friends'],
+    data() {
+        return{
+
+        }
+    },
+    methods: {
+        unfriend(name) {
+            this.$emit('delete', { name })
+        }
+    }
+}
+</script>
+
